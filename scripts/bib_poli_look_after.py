a861b0229eae8fca340e3a91b278b2d2abef6d65
Ciocirlan Stefan-Dan <stefan_dan@xn--ciocrlan-o2a.ro>
2018-10-04 09:26:02 +0200

    adding new type of look after

--- scripts/bib_poli_look_after.py
+++ scripts/bib_poli_look_after.py
@@ -10,6 +10,7 @@ import dlib
 import cv_bridge
 import json
 import math
+import time
 
 
 class LookUpLogicManager:
@@ -49,8 +50,10 @@ class LookUpLogicManager:
         self.move_stop.angular.y = 0;
         self.move_stop.angular.z = 0;
         self.distance_limit = 100;
+        self.start = time.time();
+        self.time = 2;
         rospy.sleep(3);
-        rospy.Subscriber("xtion/rgb/image_rect_color", sensor_msgs.msg.Image, self.image_subscriber_callback);
+        rospy.Subscriber("xtion/rgb/image_rect_color", sensor_msgs.msg.Image, self.image_subscriber_callback_2);
         rospy.Subscriber("bibpoli/lookup_cmd", std_msgs.msg.String, self.command_subscriber);
 
 
@@ -66,14 +69,15 @@ class LookUpLogicManager:
         y1 = point1y;
         x2 = point2x;
         y2 = point2y;
-        return math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        #return math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        return math.fabs(x1-x2);
 
     def command_subscriber(self, command):
         self.state = command.data;
         if(self.state == "START"):
             self.centered = False;
 
-    def image_subscriber_callback(self, reply):
+    def image_subscriber_callback_1(self, reply):
         if(self.state == "START"):
             frame = self.cvBridge.imgmsg_to_cv2(reply, 'bgr8');
             faces_detected = self.faceDectector(frame, 1);
@@ -108,6 +112,45 @@ class LookUpLogicManager:
             else:
                 self.move_pub.publish(self.move_right);
                 self.rate.sleep();
+
+
+    def image_subscriber_callback_2(self, reply):
+        if(self.state == "START"):
+            if time.time() - self.start > self.time:
+                frame = self.cvBridge.imgmsg_to_cv2(reply, 'bgr8');
+                faces_detected = self.faceDectector(frame, 1);
+                rospy.loginfo("[image_subscriber_callback] people detected {}".format(len(faces_detected)));
+                height, width, channels = frame.shape;
+                centerX = width / 2;
+                centerY = height / 2;
+                min_index = -1;
+                min_value = 999999;
+                for (i, rect) in enumerate(faces_detected):
+                    current_distance = self.calculate_distance(centerX, centerY, (rect.left() + rect.right())/2, (rect.bottom() + rect.top())/2);
+                    if (current_distance <  min_value):
+                        min_index = i;
+                        min_value = current_distance;
+                if(min_index != -1):
+                    rospy.loginfo("min_index {}, min_value {}".format(min_index, min_value));
+                    if (min_value > self.distance_limit):
+                        rospy.loginfo("is moving");
+                        if ( ((faces_detected[min_index].left() + faces_detected[min_index].right())/2) > centerX ):
+                            self.move_pub.publish(self.move_right);
+                            self.rate.sleep();
+                        else:
+                            self.move_pub.publish(self.move_left);
+                            self.rate.sleep();
+                        self.rep_pub.publish("NOCENTER");
+                        self.rate.sleep();
+                    else:
+                        self.move_pub.publish(self.move_stop);
+                        self.rate.sleep();
+                        self.rep_pub.publish("CENTER");
+                        self.rate.sleep();
+                else:
+                    self.move_pub.publish(self.move_right);
+                    self.rate.sleep();
+                self.start = time.time();
 
 if __name__ == '__main__':
     rospy.init_node('bibpoli_lookafter_node', anonymous=True);
